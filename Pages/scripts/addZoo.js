bc5cf52bc170eca97ba372463734d6a445a51f7e
sangar3 <55110746+sangar3@users.noreply.github.com>
2022-05-25 18:02:51 -0500

    progress on making zoo button dynamic

--- Pages/scripts/addZoo.js
+++ Pages/scripts/addZoo.js
@@ -4,6 +4,30 @@ let Zoobutton = document.getElementById("addZoo");
 
 Zoobutton.addEventListener("click", () => {
     console.log("Add zoo button was pressed")
+
+    // ADD CARD DIV 
+    const classCardDiv = document.createElement('div');
+    classCardDiv.classList.add('card')
+    document.body.appendChild(classCardDiv);
+    
+    
+    // ADD CARD BODY DIV THAT IS CHILD OF CARD DIV
+    const cardBodyDiv = document.createElement('div');
+    classCardDiv.classList.add('card-body');
+    classCardDiv.appendChild(cardBodyDiv)
+
+
+    // ADD CARD TITLE DIV THAT IS A CHILD OF CARD BODY DIV
+    const cardTitleDiv = document.createElement('h3');
+    var zooName = window.prompt("Enter your Zoo name: ");
+    const cardTitleText = document.createTextNode(zooName + " Zoo")
+    cardTitleDiv.appendChild(cardTitleText);
+    classCardDiv.classList.add('card-body');
+    cardBodyDiv.appendChild(cardTitleDiv)
+
+
+    
+    
 //     //STEP 1: Create XmlHttpRequest object
 //     let xhr = new XMLHttpRequest();
 
